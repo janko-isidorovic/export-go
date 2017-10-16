b0084f84b1533063dcf332ec177d0b8916c876d5
Federico Claramonte <fede.claramonte@caviumnetworks.com>
2017-10-16 15:15:19 +0200

    Implement distro pong Remove not used distro files
    Signed-off-by: Federico Claramonte <fede.claramonte@caviumnetworks.com>

--- distro/server.go
+++ distro/server.go
@@ -1,26 +1,24 @@
 package distro
 
 import (
+	"io"
 	"net/http"
 
 	"github.com/go-zoo/bone"
 )
 
+func replyPing(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/text; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	str := `pong`
+	io.WriteString(w, str)
+}
+
 // HTTPServer function
 func HTTPServer() http.Handler {
 	mux := bone.New()
 
-	// Status
-	mux.Get("/status", http.HandlerFunc(getStatus))
-
-	// Registration
-	mux.Get("/api/v1/registration/reference/:type", http.HandlerFunc(getRegList))
-	mux.Get("/api/v1/registration", http.HandlerFunc(getAllReg))
-	mux.Get("/api/v1/registration/name/:name", http.HandlerFunc(getRegByName))
-	mux.Post("/api/v1/registration", http.HandlerFunc(addReg))
-	mux.Put("/api/v1/registration", http.HandlerFunc(updateReg))
-	mux.Delete("/api/v1/registration/id/:id", http.HandlerFunc(delRegByID))
-	mux.Delete("/api/v1/registration/name/:name", http.HandlerFunc(delRegByName))
+	mux.Get("/api/v1/ping", http.HandlerFunc(replyPing))
 
 	return mux
 }
