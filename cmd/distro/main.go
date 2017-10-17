24f9ae6466ca5899e23d2bd0b6d15ba5f0cf5436
Federico Claramonte <fede.claramonte@caviumnetworks.com>
2017-10-17 10:42:56 +0200

    Adding some interfaces, should be moved to distro later
    Signed-off-by: Federico Claramonte <fede.claramonte@caviumnetworks.com>

--- cmd/distro/main.go
+++ cmd/distro/main.go
@@ -15,6 +15,7 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/drasko/edgex-export"
 	"github.com/drasko/edgex-export/distro"
 	"github.com/drasko/edgex-export/mongo"
 
@@ -32,6 +33,100 @@ type config struct {
 	Port     int
 	MongoURL string
 }
+
+var registrations []export.Registration
+
+type distroFormat interface {
+	Format( /*event*/ ) string
+}
+
+type distroTransformer interface {
+	Transform(data []byte) []byte
+}
+
+// type distroCompression interface {
+// 	Compress(data []byte) []byte
+// }
+
+// type distroEncryption interface {
+// 	Encrypt(data []byte) []byte
+// }
+
+type registrationInfo struct {
+	registration export.Registration
+	format       distroFormat
+	compression  distroTransformer
+	encrypt      distroTransformer
+	sender       distro.Sender
+}
+
+func (reg registrationInfo) update(newReg export.Registration) {
+	reg.registration = newReg
+	switch newReg.Format {
+	case export.FormatJSON:
+	case export.FormatXML:
+	case export.FormatSerialized:
+	case export.FormatIoTCoreJSON:
+	case export.FormatAzureJSON:
+	case export.FormatCSV:
+	default:
+	}
+	switch newReg.Compression {
+	case export.CompNone:
+	case export.CompGzip:
+	case export.CompZip:
+	default:
+	}
+	// switch newRreg.compression {
+	// case export.CompNone:
+	// case export.CompGzip:
+	// case export.CompZip:
+	// default:
+	// }
+}
+
+func sample() {
+	var sourceReg export.Registration
+	sourceReg.ID = "1"
+	sourceReg.Name = "test export"
+	// sourceReg.Addr
+	sourceReg.Format = export.FormatJSON
+	//sourceReg.Filter
+	sourceReg.Compression = export.CompNone
+	sourceReg.Encryption.Algo = export.EncNone
+	sourceReg.Enable = true
+	sourceReg.Destination = "MQTT_TOPIC"
+
+	var reg registrationInfo
+	reg.update(sourceReg)
+}
+
+// {
+// 	"_id" : ObjectId("59e4bdb9e4b091c2db2054c8"),
+// 	"_class" : "org.edgexfoundry.domain.export.ExportRegistration",
+// 	"name" : "MQTTClient2",
+// 	"addressable" : {
+// 		"_id" : null,
+// 		"name" : "FuseTestMQTTBroker2",
+// 		"protocol" : "TCP",
+// 		"address" : "tcp://127.0.0.1",
+// 		"port" : 1883,
+// 		"publisher" : "FuseExportPublisher",
+// 		"user" : "dummy",
+// 		"password" : "dummy",
+// 		"topic" : "FuseDataTopic",
+// 		"created" : NumberLong(0),
+// 		"modified" : NumberLong(0),
+// 		"origin" : NumberLong("1471806386919")
+// 	},
+// 	"format" : "JSON",
+// 	"compression" : "NONE",
+// 	"enable" : true,
+// 	"destination" : "MQTT_TOPIC",
+// 	"created" : NumberLong("1508163001830"),
+// 	"modified" : NumberLong("1508163001830"),
+// 	"origin" : NumberLong("1471806386919")
+// }
 
 func main() {
 	cfg := loadConfig()
@@ -60,6 +155,8 @@ func main() {
 		signal.Notify(c, syscall.SIGINT)
 		errs <- fmt.Errorf("%s", <-c)
 	}()
+
+	sample()
 
 	c := <-errs
 	logger.Info("terminated", zap.String("error", c.Error()))
