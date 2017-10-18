36762d40d36638309022abdaec606d62095d3a2d
Federico Claramonte <fede.claramonte@caviumnetworks.com>
2017-10-18 16:12:51 +0200

    Fixed mqtt test
    Signed-off-by: Federico Claramonte <fede.claramonte@caviumnetworks.com>

--- distro/mqtt_test.go
+++ distro/mqtt_test.go
@@ -4,11 +4,16 @@ import (
 	// "io/ioutil"
 	// "net/http"
 	"fmt"
+	"github.com/drasko/edgex-export"
 	"testing"
 )
 
 func TestMqttNew(t *testing.T) {
-	sender := NewMqttSender("tcp://127.0.0.1:1883", "", "")
+	sender := NewMqttSender(export.Addressable{
+		Address: "tcp://127.0.0.1",
+		Port:    1883,
+	})
+
 	for i := 0; i < 1000; i++ {
 
 		sender.Send(fmt.Sprintf("hola %d", i))
