bc2005453e57aba899c0c7a1d53d2c054838491d
chencho <smunoz@cavium.com>
2017-10-18 16:16:46 +0200

    Fixed http test
    Signed-off-by: chencho <smunoz@cavium.com>

--- distro/http_test.go
+++ distro/http_test.go
@@ -2,11 +2,20 @@ package distro
 
 import (
 	"fmt"
+	"github.com/drasko/edgex-export"
 	"testing"
 )
 
 func TestHttpNew(t *testing.T) {
-	sender := NewHttpSender("http://127.0.0.1")
+	addr := export.Addressable{
+		Name:     "test",
+		Method:   "GET",
+		Protocol: "HTTP",
+		Address:  "127.0.0.1",
+		Port:     80,
+		Path:     "/"}
+
+	sender := NewHttpSender(addr)
 	for i := 0; i < 1000; i++ {
 		sender.Send(fmt.Sprintf("hola %d", i))
 	}
