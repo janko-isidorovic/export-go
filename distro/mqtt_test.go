89c51fcbf8b25cc359a84fde778ea1f77db8516a
Federico Claramonte <fede.claramonte@caviumnetworks.com>
2017-10-19 11:10:27 +0200

    Use defined constants for method and protocol
    Signed-off-by: Federico Claramonte <fede.claramonte@caviumnetworks.com>

--- distro/mqtt_test.go
+++ distro/mqtt_test.go
@@ -1,13 +1,12 @@
 package distro
 
 import (
-	// "io/ioutil"
-	// "net/http"
 	"fmt"
 	"github.com/drasko/edgex-export"
 	"testing"
 )
 
+// Probably not a good test as it requires external infrastucture
 func TestMqttNew(t *testing.T) {
 	sender := NewMqttSender(export.Addressable{
 		Address: "tcp://127.0.0.1",
