1fd9c0fe4bd5846d18e5658afe0195beb65c27c6
Manuel IMPERIALE <manuel.imperiale@gmail.com>
2017-10-29 17:44:08 +0100

    Revert "Add Copyright headers"
    This reverts commit 1d384473b9ed539b53368830e9d6bb92d6b89c8d.

--- addressable.go
+++ addressable.go
@@ -1,11 +1,3 @@
-//
-// Copyright (c) Mainflux
-//
-// Mainflux server is licensed under an Apache license, version 2.0.
-// All rights not explicitly granted in the Apache license, version 2.0 are reserved.
-// See the included LICENSE file for more details.
-//
-
 package export
 
 // Protocols
