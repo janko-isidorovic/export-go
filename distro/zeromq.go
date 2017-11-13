ea461a6c0a51948a910410ca6bb3bc9205723919
Federico Claramonte <fede.claramonte@caviumnetworks.com>
2017-11-13 11:43:18 +0100

    Receiving messages from zeromq
    Signed-off-by: Federico Claramonte <fede.claramonte@caviumnetworks.com>

--- distro/zeromq.go
+++ distro/zeromq.go
@@ -1,9 +1,8 @@
 package distro
 
 import (
-	"fmt"
-
 	zmq "github.com/pebbe/zmq4"
+	"go.uber.org/zap"
 )
 
 func initZmq() {
@@ -11,17 +10,22 @@ func initZmq() {
 	defer q.Close()
 
 	logger.Info("Connecting to zmq...")
-	q.Connect("tcp://localhost:5563")
+	q.Connect("tcp://localhost:32768")
 	logger.Info("Connected to zmq")
+	q.SetSubscribe("")
 
 	for {
 		msg, err := q.RecvMessage(0)
 		logger.Info("Received zmq msg")
-		if err == nil {
+		if err != nil {
 			id, _ := q.GetIdentity()
-			fmt.Println("ERROR:", msg[0], id)
+			logger.Error("Error getting mesage", zap.String("id", id))
 		} else {
-			fmt.Println("MSG  :", msg)
+			for _, str := range msg {
+				// Why the offset of 7?? zmq v3 vs v4 ?
+				event := parseEvent(str[7:])
+				logger.Debug("Event received", zap.Any("event", event))
+			}
 		}
 	}
 }
